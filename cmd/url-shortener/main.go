073a92000e4316588d4237108a9bd708923b445b
zarixite <MrPratatip@yandex.ru>
2025-05-15 11:22:44 +0300

    config.go done

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server
+
+}
